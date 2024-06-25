4ce844df1fdeed97cd4a3914e07de94c2796284a
Wes-Git-Master <158311677+Wes-Git-Master@users.noreply.github.com>
2024-06-25 19:15:25 +0300

    lessons/routing

---
+++ src/app/comments/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const CommentsPage = () => {
+
+    return (
+        <div>
+
+            CommentsPage
+
+        </div>
+    );
+};
+
+export default CommentsPage;
---
+++ src/app/users/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const UsersPage = () => {
+
+    return (
+        <div>
+
+            UsersPage
+
+        </div>
+    );
+};
+
+export default UsersPage;
