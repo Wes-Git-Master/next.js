4b83f819e007720787811164b30d0d848c876c89
Wes-Git-Master <158311677+Wes-Git-Master@users.noreply.github.com>
2024-06-25 19:39:49 +0300

    lessons/routing

---
+++ src/app/components/MenuComponent.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Link from "next/link";
+
+const MenuComponent = () => {
+
+    return (
+        <div>
+
+            <ul>
+                <li>
+                    <Link href={'/users'}>users</Link>
+                </li>
+                <li>
+                    <Link href={'/posts'}>posts</Link>
+                </li>
+                <li>
+                    <Link href={'/comments'}>comments</Link>
+                </li>
+            </ul>
+
+        </div>
+    );
+};
+
+export default MenuComponent;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import type {Metadata} from "next";
 import {Inter} from "next/font/google";
 import "./globals.css";
+import MenuComponent from "@/app/components/MenuComponent";
 
 const inter = Inter({subsets: ["latin"]});
 
@@ -13,7 +14,10 @@ export const metadata: Metadata = {
 export default function RootLayout({children,}: Readonly<{ children: React.ReactNode; }>) {
     return (
         <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+        <MenuComponent/>
+        {children}
+        </body>
         </html>
     );
 }
