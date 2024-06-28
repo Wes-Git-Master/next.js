b2e3a5b771e78a934fdd9e535aafdbbffc258d5e
Wes-Git-Master <158311677+Wes-Git-Master@users.noreply.github.com>
2024-06-28 17:21:44 +0300

    analysis/ssr-ssg-isr

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -8,12 +8,19 @@ const urlBuilder = {
 
 const userService = {
     getAllUsers: async (): Promise<IUser[]> => {
-        let users = await fetch(urlBuilder.allUsers())
+        let users = await fetch(urlBuilder.allUsers(),{
+            // cache: 'no-cache'    - dynamic fetch
+            // cache: 'no-store'    - SSR
+            // cache: 'force-cache' - SSG
+            // next: {revalidate: 60} - incremental fetch
+
+
+        })
             .then(value => value.json());
         return users
 
     },
-    getUsersByid: async (id: string | number): Promise<IUser | null> => {
+    getUsersById: async (id: string | number): Promise<IUser | null> => {
         let user: IUser = await fetch(urlBuilder.singleUser(id))
             .then(value => value.json());
         return user
